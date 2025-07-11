860e2d22fc42fac06b2958d8ef44e283c6dc2354
yijean333 <poison02580@gmail.com>
2025-07-11 16:00:23 +0800

    Update api.py

--- api.py
+++ api.py
@@ -7,19 +7,19 @@ app = Flask(__name__)
 with open('violations.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
 
-violations = data.get("test_road", [])
+violations = data  # 把整包都拿來用
 
 @app.route("/violations/<camera_name>", methods=["GET"])
 def get_by_camera(camera_name):
     today_only = request.args.get("today") == "1"
 
-    filtered = [v for v in violations if v["camera_name"] == camera_name]
+    records = violations.get(camera_name, [])
 
     if today_only:
         today = datetime.now().date()
-        filtered = [
-            v for v in filtered
+        records = [
+            v for v in records
             if datetime.strptime(v["time"], "%Y-%m-%d %H:%M:%S").date() == today
         ]
 
-    return jsonify(filtered)
+    return jsonify(records)
